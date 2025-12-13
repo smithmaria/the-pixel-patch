4d74e03f380d12a124fa7d7c236b76bc1c68f0ff
Maria Smith <maria.t.smith023@gmail.com>
2025-12-12 22:13:12 -0500

    navigation, old edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navigation } from './components/Navigation'
+import { Home } from './components/Home'
+import { RockPaperScissors } from './games/rock-paper-scissors/RockPaperScissors'
+import { TicTacToe } from './games/tic-tac-toe/TicTacToe'
+import { Wordle } from './games/wordle/Wordle'
+
 function App() {
 
   return (
-    <>
-      <h1>GameHub</h1>
-      <p>Welcome to GameHub</p>
-    </>
+    <BrowserRouter>
+      <div className="app">
+        <Navigation />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/rps" element={<RockPaperScissors />}/>
+            <Route path="/tictactoe" element={<TicTacToe />}/>
+            <Route path="/wordle" element={<Wordle />}/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+import './navigation.css'
+
+export function Navigation() {
+  return (
+    <nav className='navbar'>
+      Hello.
+    </nav>
+  )
+}
