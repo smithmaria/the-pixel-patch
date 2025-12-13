4977458a7dd2548b9e12b7e2492cc754bca48c93
Maria Smith <maria.t.smith023@gmail.com>
2025-12-13 01:57:43 -0500

    fix game screen navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,44 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './navigation.css'
 
 import logo from '../../shared/assets/pixel-patch-logo.png';
 import menuIcon from '../../shared/assets/menu-bars.svg';
 
+const gameTitles = {
+  '/rps': 'Rock, Paper, Scissors',
+  '/tictactoe': 'Tic Tac Toe',
+  '/wordle': 'Wordle',
+  '/sugarrush': 'Sugar Rush'
+};
+
 export function Navigation() {
 const location = useLocation();
 const isHomePage = location.pathname === '/';
+const gameTitle = gameTitles[location.pathname];
 
   return (
     <nav className='navbar'>
-      <div className={`${isHomePage ? 'navbar-home' : 'navbar-game'}`}>
+      <div className='navbar-container'>
         {isHomePage && (
           <div></div>
         )}
-  
-        <div className={isHomePage ? 'logo-home' : 'logo-game'}>
-          <img src={logo} alt='Pixel Patch Logo'/>
-        </div>
+        
+        {isHomePage && (
+          <div className='logo-home'>
+            <img src={logo} alt='Pixel Patch Logo'/>
+          </div>
+        )}
+
+        {!isHomePage && (
+          <>
+            <Link to='/'>
+              <div className='logo-game'>
+                <img src={logo} alt='Pixel Patch Logo'/>
+              </div>
+            </Link>
+            <div className='game-title'>{gameTitle}</div>
+          </>
+        )}
 
         <div className='menu-icon'>
           <img src={menuIcon} alt='Menu'/>
