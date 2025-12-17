00ac5773500c2cea5fee1cc54308e883561ea1f0
Maria Smith <maria.t.smith023@gmail.com>
2025-12-17 17:35:15 -0500

    update vite config to new repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'final-project-maria-smith',
+  base: 'the-pixel-patch',
   plugins: [react()],
   server: { port: 5173, open: false },
   preview: { port: 5173 },
