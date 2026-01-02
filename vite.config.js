f2b935737e652929e099b1132bf8626c4b9a6571
Maria Smith <maria.t.smith023@gmail.com>
2026-01-02 18:58:43 -0500

    remove base field from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/the-pixel-patch/',
   plugins: [react()],
   server: { port: 5173, open: false },
   preview: { port: 5173 },
