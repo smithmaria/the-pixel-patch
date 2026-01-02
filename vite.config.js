bb6fe7c169a829612d531483240293714dcbceac
Maria Smith <maria.t.smith023@gmail.com>
2026-01-02 18:53:25 -0500

    add slashes to base in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'the-pixel-patch',
+  base: '/the-pixel-patch/',
   plugins: [react()],
   server: { port: 5173, open: false },
   preview: { port: 5173 },
